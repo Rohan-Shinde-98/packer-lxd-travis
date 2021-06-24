384da23748541f8239f2a6d9bd2a3ecdc239d328
Rohan-Shinde-98 <82387052+Rohan-Shinde-98@users.noreply.github.com>
2021-06-24 13:53:15 +0530

    Update file.sh

--- file.sh
+++ file.sh
@@ -1,15 +1,40 @@
 cat <<EOF | sudo lxd init --preseed
 config:
 #   core.https_address: 192.168.1.1:9999
-  images.auto_update_interval: 15
+  core.trust_password: sekret
+  images.auto_update_interval: 6
+
+# Storage pools
+storage_pools:
+- name: data
+  driver: zfs
+  config:     
+    source: my-zfs-pool/my-zfs-dataset
+
+# Network devices
 networks:
-- name: lxdbr0
+- name: lxd-my-bridge
   type: bridge
   config:
     ipv4.address: auto
     ipv6.address: none
-root:
-  type: disk
-  path: /
-  pool: default
+
+# Profiles
+profiles:
+- name: default
+  devices:
+    root:
+      path: /
+      pool: data
+      type: disk
+- name: test-profile
+  description: "Test profile"
+  config:
+    limits.memory: 2GB
+  devices:
+    test0:
+      name: test0
+      nictype: bridged
+      parent: lxd-my-bridge
+      type: nic
 EOF
