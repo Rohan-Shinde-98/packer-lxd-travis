fc42c5e4376c85d43fb5836fb4d3da38cb7c67f4
Rohan-Shinde-98 <82387052+Rohan-Shinde-98@users.noreply.github.com>
2021-06-24 13:24:20 +0530

    Update file.sh

--- file.sh
+++ file.sh
@@ -1,6 +1,6 @@
 cat <<EOF | sudo lxd init --preseed
 config:
-  core.https_address: 192.168.1.1:9999
+#   core.https_address: 192.168.1.1:9999
   images.auto_update_interval: 15
 networks:
 - name: lxdbr0
