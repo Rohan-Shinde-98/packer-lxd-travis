cc2d4d02bb77ad36fc8a24fa88ff37a500e88272
Rohan-Shinde-98 <82387052+Rohan-Shinde-98@users.noreply.github.com>
2021-06-24 13:20:27 +0530

    Create file.sh

---
+++ file.sh
@@ -0,0 +1,11 @@
+cat <<EOF | lxd init --preseed
+config:
+  core.https_address: 192.168.1.1:9999
+  images.auto_update_interval: 15
+networks:
+- name: lxdbr0
+  type: bridge
+  config:
+    ipv4.address: auto
+    ipv6.address: none
+EOF
